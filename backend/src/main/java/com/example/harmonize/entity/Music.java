8fe5ec36b6e427e7f7f7a4975cd5a09890b6e8a8
sori9899 <hyun989969@gmail.com>
2023-04-26 16:30:43 +0900

    init Music

--- backend/src/main/java/com/example/harmonize/entity/Music.java
+++ backend/src/main/java/com/example/harmonize/entity/Music.java
@@ -1,8 +1,27 @@
 package com.example.harmonize.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
+@Entity
+@Table(name = "music")
 public class Music {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "music_id")
+    private Long music_id;
+
+    private String music_name;
+    private String artist;
+    private Integer gender;
+    private Integer tiem;
+    private String image_link;
+    private Integer music_numberTJ;
+    private String youtube_link;
+    private String category;
+    private String filename;
+    private Double max;
+    private Double min;
+    private Integer level;
 }
