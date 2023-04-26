22786e1c3ec70131251eeb3cad998a3bc968f30f
Woochang Lee <changi112242@outlook.com>
2023-04-26 19:07:03 +0900

    Implement MoreFragment basic UI

--- android/app/src/main/java/kr/ac/chungbuk/harmonize/ui/more/MoreFragment.java
+++ android/app/src/main/java/kr/ac/chungbuk/harmonize/ui/more/MoreFragment.java
@@ -4,17 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.util.ArrayList;
+
+import kr.ac.chungbuk.harmonize.R;
 import kr.ac.chungbuk.harmonize.databinding.FragmentMoreBinding;
+import kr.ac.chungbuk.harmonize.item.MoreItemView;
+import kr.ac.chungbuk.harmonize.item.SearchHistoryItemView;
 
 public class MoreFragment extends Fragment {
 
     private FragmentMoreBinding binding;
+
+    ListView menuListView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +33,15 @@ public class MoreFragment extends Fragment {
         binding = FragmentMoreBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        moreViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        MoreMenuAdapter adapter = new MoreMenuAdapter();
+        adapter.addItem("관심 장르 수정");
+        adapter.addItem("성별/연령대 수정");
+        adapter.addItem("도움말");
+        adapter.addItem("로그아웃");
+
+        menuListView = (ListView) root.findViewById(R.id.menuListView);
+        menuListView.setAdapter(adapter);
+
         return root;
     }
 
@@ -33,5 +49,35 @@ public class MoreFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    class MoreMenuAdapter extends BaseAdapter {
+        ArrayList<String> menus = new ArrayList<>();
+
+        @Override
+        public int getCount() {
+            return menus.size();
+        }
+
+        public void addItem(String text) {
+            menus.add(text);
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return menus.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            MoreItemView view = new MoreItemView(getActivity().getApplicationContext());
+            view.setMenuName(menus.get(position));
+            return view;
+        }
     }
 }
