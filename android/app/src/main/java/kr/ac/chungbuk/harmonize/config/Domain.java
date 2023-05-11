bfdbb1f39ceb74702d6adbd9955819caf701a87c
Woochang Lee <changi112242@outlook.com>
2023-05-11 11:27:04 +0900

    Add login request test code using Volley

---
+++ android/app/src/main/java/kr/ac/chungbuk/harmonize/config/Domain.java
@@ -0,0 +1,14 @@
+package kr.ac.chungbuk.harmonize.config;
+
+public class Domain {
+    public static String name = "http://10.0.2.2:9090";
+
+    /**
+     * @param path 도메인 이름 이후 "/"로 시작하는 경로
+     * @return 도메인 주소 + 경로
+     */
+    public static String url(String path)
+    {
+        return name + path;
+    }
+}
--- android/app/src/main/java/kr/ac/chungbuk/harmonize/ui/user/LoginActivity.java
+++ android/app/src/main/java/kr/ac/chungbuk/harmonize/ui/user/LoginActivity.java
@@ -7,13 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.QuickContactBadge;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 import kr.ac.chungbuk.harmonize.R;
+import kr.ac.chungbuk.harmonize.config.Domain;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button btnSignup;
+    RequestQueue queue;
+
+    Button btnSignup, btnLogin;
+    TextInputEditText editTextId, editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +40,11 @@ public class LoginActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+
+        queue = Volley.newRequestQueue(this);
+
+        editTextId = findViewById(R.id.editTextId);
+        editTextPassword = findViewById(R.id.editTextPassword);
 
         btnSignup = findViewById(R.id.btnSignup);
         btnSignup.setOnClickListener(new View.OnClickListener() {
@@ -30,5 +54,66 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                login(editTextId.getText().toString(), editTextPassword.getText().toString());
+            }
+        });
+
+    }
+
+    private void login(String username, String password)
+    {
+        StringRequest request = new StringRequest(Request.Method.POST, Domain.url("/api/login"),
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // TO-DO : 토큰과 user 정보를 저장하기
+                        btnSignup.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        btnSignup.setText(error.toString());
+                    }
+                }
+        ) {
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                try {
+                    String utf8String = new String(response.data, "UTF-8");
+                    return Response.success(utf8String, HttpHeaderParser.parseCacheHeaders(response));
+                } catch (UnsupportedEncodingException e) {
+                    // log error
+                    return Response.error(new ParseError(e));
+                } catch (Exception e) {
+                    // log error
+                    return Response.error(new ParseError(e));
+                }
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                //params.put("Content-Type", "application/json; charset=UTF-8");
+                //params.put("token", "welkfjlwejflwe");
+                return params;
+            }
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String,String>();
+                params.put("username", username);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        queue.add(request);
     }
 }
