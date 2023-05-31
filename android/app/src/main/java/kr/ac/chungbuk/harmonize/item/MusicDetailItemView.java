50c95b0e8514e112414b994dc196fc72045e1fee
Woochang Lee <changi112242@outlook.com>
2023-05-31 18:39:30 +0900

    Add music play function

--- android/app/src/main/java/kr/ac/chungbuk/harmonize/item/MusicDetailItemView.java
+++ android/app/src/main/java/kr/ac/chungbuk/harmonize/item/MusicDetailItemView.java
@@ -3,9 +3,11 @@ package kr.ac.chungbuk.harmonize.item;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.media.MediaPlayer;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -13,6 +15,8 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
@@ -23,8 +27,16 @@ import kr.ac.chungbuk.harmonize.utility.PitchConverter;
 
 public class MusicDetailItemView extends LinearLayout {
 
-    TextView tvName, tvArtist, tvMaxPitch, tvMinPitch, tvDifficulty, tvTJNum, tvHighRate, tvLowRate;
+    Long musicId;
+
+    RequestQueue queue;
+    private MediaPlayer mediaPlayer;
+    int position = 0;
+
+    TextView tvName, tvArtist, tvMaxPitch, tvMinPitch, tvDifficulty,
+            tvTJNum, tvHighRate, tvLowRate, tvCover;
     ImageView thumbnailView;
+    ImageButton ibPlay, ibPause, ibStop;
 
     public MusicDetailItemView(Context context) {
         super(context);
@@ -40,6 +52,8 @@ public class MusicDetailItemView extends LinearLayout {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.music_detail_item, this, true);
 
+        queue = Volley.newRequestQueue(getContext());
+
         tvName = findViewById(R.id.tvName);
         tvArtist = findViewById(R.id.tvArtist);
         thumbnailView = findViewById(R.id.thumbnailView);
@@ -49,10 +63,60 @@ public class MusicDetailItemView extends LinearLayout {
         tvTJNum = findViewById(R.id.tvTJNum);
         tvHighRate = findViewById(R.id.tvHighRate);
         tvLowRate = findViewById(R.id.tvLowRate);
+        tvCover = findViewById(R.id.tvCover);
+
+        ibPlay = findViewById(R.id.ibPlay);
+        ibPause = findViewById(R.id.ibPause);
+        ibStop = findViewById(R.id.ibStop);
+
+        ibPlay.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playMusic();
+            }
+        });
+
+        ibPause.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer != null) {
+                    position = mediaPlayer.getCurrentPosition();
+                    mediaPlayer.pause();
+                }
+            }
+        });
+
+        ibStop.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                    mediaPlayer = null;
+                }
+            }
+        });
     }
+
+    private void playMusic() {
+        try {
+            if (mediaPlayer != null){
+                mediaPlayer.start();
+            }
+
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(Domain.url("/music/m4a/" + musicId));
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+        } catch (Exception e) {}
+    }
+
+
 
     public void loadMusicDetail(MusicDetail music)
     {
+        musicId = music.music_id;
         tvName.setText(music.music_name);
         tvArtist.setText(music.artist);
         tvMaxPitch.setText(PitchConverter.doubleToPitch(music.max));
@@ -61,6 +125,7 @@ public class MusicDetailItemView extends LinearLayout {
         tvTJNum.setText("TJ [" + music.tj_num + "]");
         tvHighRate.setText(music.high + "%");
         tvLowRate.setText(music.low + "%");
+        tvCover.setText(music.range_avg + "% 커버");
 
         Glide
                 .with(getContext())
